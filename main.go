08a334541892c3f4b49987ad40ae707d4051e052
Siming Liu <siming.liu@dbjtech.com>
2020-11-05 11:28:17 +0800

    try

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 )
 
 func main() {
+	a, b := 5, 5
+	c := 6 / (a - b)
+	_ = c
 	fmt.Println(">- .test github action. -<")
 	fmt.Println("> --- everything is ok ---")
 	fmt.Println("> ...    -exiting-     .. ")
